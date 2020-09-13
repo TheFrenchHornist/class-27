34cfc8dd88a21765a255eb7128d4d0fd8892369c
TheFrenchHornist <lexpayne007@gmail.com>
2020-09-13 13:02:55 -0400

    today we learned about constrained bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var fishingRod;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    fishingRod = new Chain(bird.body, pig3.body);
 }
 
 function draw(){
@@ -61,4 +64,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    fishingRod.display();
 }
